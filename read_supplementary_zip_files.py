1e742c3bb64ef219fc2819246ded68a44a94d67c
Gergely Attila Kiss <corra971407@gmail.com>
2019-03-22 17:15:14 +0100

    solve zip problem (maybe) and flag with every script type

--- read_supplementary_zip_files.py
+++ read_supplementary_zip_files.py
@@ -1,8 +1,8 @@
 from glob import glob
-import re
-from zipfile import ZipFile
+import re, csv
+from zipfile import ZipFile, BadZipFile
 from io import BytesIO
-import os
+import os, os.path
 
 files = glob('REStud papers/*/*ppl*.zip')
 
@@ -18,11 +18,14 @@ EXTENSIONS={"Python": ".py", "Stata":".do", "R": ".r", "MatLab": ".m", "C": ".c"
 FLAGS={"Python":0, "Stata":0, "R":0, "MatLab":0, "C":0, "Fortran":0, "bash":0}
 #store the flags given to zips
 
+LANGUAGE={"ms_number":"","Python":False, "Stata":False, "R":False, "MatLab":False, "C":False, "Fortran":False,"Bash":False}
+#support dictionary to write rows
+
 def count_extensions(name, extensions, nr_extensions):
         for key in nr_extensions:
             if key == "Fortran" and (os.path.splitext(name)[1][:2] == extensions[key] ):
                 nr_extensions[key]+=1
-               
+            #count problems if there are any .f* extensions except fortran
             elif os.path.splitext(name)[1] == extensions[key] :
                 nr_extensions[key]+=1
 #stock extensions in nr_extensions
@@ -37,27 +40,46 @@ def tree_in_zip(path, stream=None):
         zf = ZipFile(stream)
     else:
         zf = ZipFile(path)
-    for name in zf.namelist():
-    	reset_extension_nr(NR_EXTENSIONS)
-    	count_extensions(name, EXTENSIONS, NR_EXTENSIONS)
-        if ZIP_FILE.search(name) is not None:
-            # FIXME: does it work 2-3 deep? zip in zip in zip?
-            print('-Found {}'.format(name))
-            yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))
-        else:
-            yield path+'/'+name
+    reset_extension_nr(NR_EXTENSIONS)
+    try:
+        for name in zf.namelist(): 
+            count_extensions(name, EXTENSIONS, NR_EXTENSIONS)
+            if ZIP_FILE.search(name) is not None:
+                # FIXME: does it work 2-3 deep? zip in zip in zip? | FIXED!(maybe)
+                print('-Found {}'.format(name))
+                yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))
+            else:
+                yield path+'/'+name
+    except BadZipFile :
+        pass
 
 def flager(name, nr_extensions, flags):
+	format_list=['','','']
     format_list[0]=os.path.splitext(name)[0]
-    format_list[1]=max(nr_extensions, key=nr_extensions.get)
     format_list[2]=os.path.splitext(name)[1]
-    name=os.rename(name,'{}_{}{}'.format(*format_list))
-    #extends zip files with a flag of most used script files
-    flags[max(nr_extensions, key=nr_extensions.get)]+=1 
-    #counts the flags given
-
+    if max(nr_extensions.values()) == 0 :
+        name=os.rename(name,name) 
+        #leaves the name unchanged if there's no script
+    else :
+        i=0
+        sort=(sorted(nr_extensions, key=nr_extensions.__getitem__, reverse=True))
+        while nr_extensions[sort[i]]!=0 :
+            format_list[1]=format_list[1]+'_'+sort[i]
+            flags[sort[i]]+=1 
+            #count flags given
+            language[sort[i]]=True 
+            #set for the row to wirte out
+            i+=1
+        name=os.rename(name,'{}{}{}'.format(format_list[0],format_list[1],format_list[2])) 
+        #flags zip with every used script
 
 for name in files:
     for entry in tree_in_zip(name):
         print(entry)
     flager(name, NR_EXTENSIONS, FLAGS)
+################################################################################################
+####LANGUAGE["ms_number"]=MS_ID.match(name)
+####writer = csv.DictWriter(open('MS_language.csv', 'w'), fieldnames=list(LANGUAGE.keys()))
+####writer.writeheader()
+####writer.writerow(LANGUAGE)
+# under construction
