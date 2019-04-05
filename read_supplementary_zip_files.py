097b93cdc05763be7aa557d5d286111a7e40be8f
Gergely Attila Kiss <corra971407@gmail.com>
2019-04-05 13:27:09 +0200

    solve .tar reading problems

--- read_supplementary_zip_files.py
+++ read_supplementary_zip_files.py
@@ -1,33 +1,31 @@
 from glob import glob
 import re, csv
 from zipfile import ZipFile, BadZipFile
+from gzip import GzipFile
+from tarfile import TarFile, ReadError
 from io import BytesIO
-import os, os.path
+import os
 
 files = glob('REStud papers/*/*ppl*.zip')
 
 MS_ID = re.compile(r'[12][0-9]{4}')
 ZIP_FILE = re.compile(r'\.zip$', flags=re.IGNORECASE)
+TAR_FILE=re.compile(r'\.tar', flags=re.IGNORECASE)
 
-NR_EXTENSIONS={"Python":0, "Stata":0, "R":0, "MatLab":0, "C":0, "Fortran":0, "bash":0}
+NR_EXTENSIONS={ "ms_number": 0,"Python":0, "Stata":0, "R":0, "MatLab":0, "C":0, "Fortran":0, "Bash":0, "LaTeX": 0, "C++": 0, "SAS":0}
 #store number of used extensions
 
-EXTENSIONS={"Python": ".py", "Stata":".do", "R": ".r", "MatLab": ".m", "C": ".c", "Fortran": ".f", "bash": ".sh"}
+EXTENSIONS={"ms_number": "0", "Python": ".py", "Stata":".do", "R": ".R", "MatLab": ".m", "C": ".c", "Fortran": re.compile(r'[.][f][0-9]{2}'), "Bash": ".sh", "LaTeX": ".tex", "C++": ".cpp", "SAS": ".sas"}
 #extension ids
 
-FLAGS={"Python":0, "Stata":0, "R":0, "MatLab":0, "C":0, "Fortran":0, "bash":0}
-#store the flags given to zips
-
-LANGUAGE={"ms_number":"","Python":False, "Stata":False, "R":False, "MatLab":False, "C":False, "Fortran":False,"Bash":False}
-#support dictionary to write rows
 
 def count_extensions(name, extensions, nr_extensions):
-        for key in nr_extensions:
-            if key == "Fortran" and (os.path.splitext(name)[1][:2] == extensions[key] ):
-                nr_extensions[key]+=1
-            #count problems if there are any .f* extensions except fortran
-            elif os.path.splitext(name)[1] == extensions[key] :
-                nr_extensions[key]+=1
+    ext=os.path.splitext(name)[1]
+    for key in nr_extensions:
+        if key == "Fortran" and extensions["Fortran"].search(ext) is not None:
+            nr_extensions[key]+=1
+        elif ext == extensions[key] :
+            nr_extensions[key]+=1
 #stock extensions in nr_extensions
 
 def reset_extension_nr(extensions):
@@ -36,50 +34,72 @@ def reset_extension_nr(extensions):
 
 def tree_in_zip(path, stream=None):
     print('Trying {}'.format(path))
-    if stream is not None:
+    if stream is not None and ZIP_FILE.search(path) is not None:
         zf = ZipFile(stream)
-    else:
-        zf = ZipFile(path)
-    reset_extension_nr(NR_EXTENSIONS)
-    try:
-        for name in zf.namelist(): 
-            count_extensions(name, EXTENSIONS, NR_EXTENSIONS)
-            if ZIP_FILE.search(name) is not None:
-                # FIXME: does it work 2-3 deep? zip in zip in zip? | FIXED!(maybe)
-                print('-Found {}'.format(name))
-                yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))
-            else:
-                yield path+'/'+name
-    except BadZipFile :
-        pass
+        #Check if the file is not the first in the loop and wheter it is a .zip.
 
-def flager(name, nr_extensions, flags):
-	format_list=['','','']
-    format_list[0]=os.path.splitext(name)[0]
-    format_list[2]=os.path.splitext(name)[1]
-    if max(nr_extensions.values()) == 0 :
-        name=os.rename(name,name) 
-        #leaves the name unchanged if there's no script
-    else :
-        i=0
-        sort=(sorted(nr_extensions, key=nr_extensions.__getitem__, reverse=True))
-        while nr_extensions[sort[i]]!=0 :
-            format_list[1]=format_list[1]+'_'+sort[i]
-            flags[sort[i]]+=1 
-            #count flags given
-            language[sort[i]]=True 
-            #set for the row to wirte out
-            i+=1
-        name=os.rename(name,'{}{}{}'.format(format_list[0],format_list[1],format_list[2])) 
-        #flags zip with every used script
+    elif stream is None and ZIP_FILE.search(path) is not None:
+        zf = ZipFile(path)
+        #Check if the first file is .zip.
+    else:
+        if stream is not None and TAR_FILE.search(path) is not None :
+            try: 
+                zf=TarFile.open(fileobj=stream, encoding="utf-8",mode='r:*')
+            except ReadError:
+                pass
+            #If the file is not the first in the loop and it is a .tar(.xx) tries to read it as an archive.
+        elif stream is None and TAR_FILE.search(path) is not None:
+            try:
+                zf=TarFile.open(path, mode='r:*')
+            except ReadError:
+                pass
+            #If the first file is a .tar(.xx) tries to read it as an archive.
+    try:
+        
+        if type(zf)==ZipFile:
+            try:
+                for name in zf.namelist(): 
+                    count_extensions(name, EXTENSIONS, NR_EXTENSIONS)
+                    #Count extensions of interest in the archive.
+                    if ZIP_FILE.search(name) is not None:
+                        print('-Found {}'.format(name))
+                        yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))
+                    elif TAR_FILE.search(name) is not None:
+                        print('-Found {}'.format(name))
+                        yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))
+                    else:
+                        yield path+'/'+name
+            except BadZipFile:
+                pass
+            #Search for further archives (.zip/.tar). Exception needed to not to stop at a corrupted archive.
+        elif type(zf)==TarFile:
+            #No need for try checked the file at the begining.
+                for name in zf.getnames():
+                    count_extensions(name, EXTENSIONS, NR_EXTENSIONS)
+                    if ZIP_FILE.search(name) is not None:
+                        print('-Found {}'.format(name))
+                        yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))
+                    elif TAR_FILE.search(name) is not None:
+                        print('- Found {}'.format(name))
+                        yield from tree_in_zip(path+'/'+name, BytesIO(zf.read(name)))    
+                    else:
+                        yield path+'/'+name
+        else:
+            pass
+        #if file is not .tar/.zip skip it
+    except UnboundLocalError:
+        pass
+    #Because of the .tar checks at the begining could happen that there's no zf defined. This way the loop goes on if found a corrupted archive.
+
+writer = csv.DictWriter(open('MS_language_test.csv', 'w'), fieldnames=list(NR_EXTENSIONS.keys()))
+writer.writeheader()
 
 for name in files:
+    reset_dictionary(NR_EXTENSIONS)
+    #Must reset counting outside the functions, because we write in this loop.
     for entry in tree_in_zip(name):
         print(entry)
-    flager(name, NR_EXTENSIONS, FLAGS)
-################################################################################################
-####LANGUAGE["ms_number"]=MS_ID.match(name)
-####writer = csv.DictWriter(open('MS_language.csv', 'w'), fieldnames=list(LANGUAGE.keys()))
-####writer.writeheader()
-####writer.writerow(LANGUAGE)
-# under construction
+    NR_EXTENSIONS["ms_number"]=MS_ID.search(name).group()
+    print(NR_EXTENSIONS)
+    #Show the row we write in .csv.
+    writer.writerow(NR_EXTENSIONS)
